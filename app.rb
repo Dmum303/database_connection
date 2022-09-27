ae53bd6e75d7f9da75a5a269ea49d04d4e8fb0bc
David Thomson <dave.t303@gmail.com>
2022-09-27 13:09:00 +0100

    Project with example files and tests working

---
+++ app.rb
@@ -0,0 +1,26 @@
+# file: app.rb
+
+require_relative 'lib/database_connection.rb'
+require_relative 'lib/Artist_repository'
+
+# We need to give the database name to the method `connect`.
+DatabaseConnection.connect('music_library')
+
+#This code not needed as now in Artist_repository file
+# Perform a SQL query on the database and get the result set.
+# sql = 'SELECT id, title FROM albums;'
+# result = DatabaseConnection.exec_params(sql, [])
+
+# Print out each record from the result set .
+# result.each do |record|
+#   p record
+# end
+
+artist_repository = ArtistRepository.new
+
+# p artist_repository.all
+
+# Cleaner output
+artist_repository.all.each do |artist|
+    p artist
+end
---
+++ spec/seeds_artists.sql
@@ -0,0 +1,15 @@
+-- (file: spec/seeds_{table_name}.sql)
+
+-- Write your SQL seed here. 
+
+-- First, youd need to truncate the table - this is so our table is emptied between each test run,
+-- so we can start with a fresh state.
+-- (RESTART IDENTITY resets the primary key)
+
+TRUNCATE TABLE artists RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO artists (name, genre) VALUES('Pixies', 'Rock');
+INSERT INTO artists (name, genre) VALUES('ABBA', 'Pop');
